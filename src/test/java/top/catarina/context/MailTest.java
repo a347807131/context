de1eedf2acb029599dd886c722024f56c36884e8
a347807131 <947124233@qq.com>
2018-01-11 23:05:47 +0800

    Initial commit

---
+++ src/test/java/top/catarina/context/MailTest.java
@@ -0,0 +1,13 @@
+package top.catarina.context;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:bean.xml")
+public class MailTest {
+	public void test1(){
+
+	}
+}
